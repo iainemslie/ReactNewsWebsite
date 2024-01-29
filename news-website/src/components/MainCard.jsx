2dacb868e819fdf52fe4fc768a6ac67a01798ade
Iain Emslie <iainemslie@gmail.com>
2024-01-29 15:14:41 -0500

    refined main card

--- news-website/src/components/MainCard.jsx
+++ news-website/src/components/MainCard.jsx
@@ -7,8 +7,9 @@ const MainCard = () => {
         src='https://images.pexels.com/photos/1267696/pexels-photo-1267696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
         alt='two ladies laughing'
       />
-      <h4 className='card-category'>Category</h4>
-      <h2 className='card-title'>Headline Text</h2>
+      <h4 className='card-category'>Entertainment</h4>
+      <h2 className='card-title'>Top 10 Tips for Day Drinking The Right Way</h2>
+      <h5 className='card-author'>Sally Struthers | 2024-01-29</h5>
       <p className='card-description'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nam
         excepturi nulla ducimus similique reiciendis minima non architecto quam
