dd2107e1558b7b04d42491cbb596896de5624ffb
Iain Emslie <iainemslie@gmail.com>
2024-01-30 08:32:31 -0500

    header is nice

--- news-website/src/components/Hero.jsx
+++ news-website/src/components/Hero.jsx
@@ -1,11 +1,8 @@
 import LargeImageCard from './LargeImageCard';
+import SmallImageCard from './SmallImageCard';
 
 const Hero = () => {
-  return (
-    <div className='hero container'>
-      <LargeImageCard />
-    </div>
-  );
+  return <div className='hero container'></div>;
 };
 
 export default Hero;
---
+++ news-website/src/components/SmallImageCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SmallImageCard = () => {
+  return (
+    <div className='small-card'>
+      <h2 className='card-title'>
+        Top 10 Tips for Day Drinking The Right Way This Summer
+      </h2>
+      <h5 className='card-author'>Sally Struthers</h5>
+    </div>
+  );
+};
+
+export default SmallImageCard;
