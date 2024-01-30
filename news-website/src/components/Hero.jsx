a47710d4bf8765086623ddc97061c61a656fa385
Iain Emslie <iainemslie@gmail.com>
2024-01-30 08:59:03 -0500

    fix hero layout

--- news-website/src/components/Hero.jsx
+++ news-website/src/components/Hero.jsx
@@ -4,7 +4,25 @@ import SmallImageCard from './SmallImageCard';
 const Hero = () => {
   return (
     <div className='container'>
-      <div className='hero'></div>
+      <div className='hero'>
+        <div className='hero-left'>
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+        </div>
+        <div className='hero-middle'>
+          <LargeImageCard />
+        </div>
+        <div className='hero-right'>
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+          <SmallImageCard />
+        </div>
+      </div>
     </div>
   );
 };
