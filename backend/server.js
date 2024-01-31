e4b1ccdec25affa74f869285809d42d3b21c9025
Iain Emslie <iainemslie@gmail.com>
2024-01-31 10:03:27 -0500

    add mongodb connection on localhost

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const port = process.env.port || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.port || 5000;
+
+connectDB();
 
 const app = express();
-
-app.use(errorHandler);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/articles', require('./routes/articleRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
